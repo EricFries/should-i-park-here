fce0167db09a97d13b8c9fe7bda52fc9327b6eb8
EricFries <ericfries@gmail.com>
2015-03-31 11:03:11 -0400

    add search again button and implement jquery to show/hide form

--- app/assets/javascripts/violation.js
+++ app/assets/javascripts/violation.js
@@ -1,4 +1,15 @@
 $(function(){
+
+	hideSearchAgain();
+
+	$("#search-again").click(function(){
+		showForm();
+		hideSearchAgain();
+		$("#new_violation").trigger("reset");
+		$(".search-result").empty();
+	});
+
+
 	$("#new_violation").on("submit", function(e){
 		e.preventDefault();
 		var url = $(this).attr("action");
@@ -10,16 +21,34 @@ $(function(){
 			success: function(response){
 				hideForm();
 				displayViolation(response);
-				
+				showSearchAgain();
 			}
 		});
 	});
 });
 
 function displayViolation(response){
-	$("body").append(response);
+	$(".search-result").append(response);
 }
 
 function hideForm(){
 	$("#new_violation").slideUp("fast");
 }
+
+function showForm(){
+	$("#new_violation").show();
+}
+
+function hideSearchAgain(){
+	$("#search-again").hide();
+}
+
+function showSearchAgain(){
+	$("#search-again").show();
+}
+
+// Add button to display search form again.
+// Mabye just hide it initially and show it once a search is completed.
+// function displaySearchAgain(){
+
+// }
