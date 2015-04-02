537cf1641b21a7bfc74d62d641f77976842dd532
EricFries <ericfries@gmail.com>
2015-04-02 09:24:08 -0400

    delete twitter embed

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-    client = Twitter::REST::Client.new do |config|
+CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key        = "RlRdicGPlBPenMEtomcir3T8b"
   config.consumer_secret     = "lZqDPgJ1ZyGcBqLNlytiMlO6b0UybyobBsrUxkPid70EiJ52CW"
   config.access_token        = "1483431355-obaCmNEE2XRmucG09wY3QRvfedSMNe4VCc2ko9w"
