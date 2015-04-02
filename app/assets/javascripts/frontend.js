a9674a610c85ed477acc18a2e380048b0237a8cc
EricFries <ericfries@gmail.com>
2015-04-02 17:20:42 -0400

    clean up application.html.erb and move js script to frontend.js

---
+++ app/assets/javascripts/frontend.js
@@ -0,0 +1,33 @@
+		$(function() {
+					
+				setTimeout(function(){
+				
+					$('.fill-limit').each(function() {
+						var me = $(this);
+						var perc = me.attr("data-limit");
+						var current_perc = 10;
+						
+						if(!$(this).hasClass('stop')){
+						
+							var progress = setInterval(function() {
+															
+								if (current_perc>=perc) {
+									clearInterval(progress);
+								} else {
+									current_perc +=1;
+									me.parent().children().children('.filler').css('height', (current_perc)+'%');
+									me.html(current_perc+'<span>%</span>');
+								}
+						
+							}, 6);
+							
+							me.addClass('stop');
+							me.parent().children().children('.filler').addClass('stop');
+							
+						}
+						
+					});
+					
+				}, 0);
+				
+			});
