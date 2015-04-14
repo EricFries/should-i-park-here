b0fdaa715279dbbe5d2e7af5419ab2c8ead9409e
ktravers <travers.kt@gmail.com>
2015-04-14 01:34:55 -0400

    update send_sms_spec, not passing

--- spec/features/search_parking_spec.rb
+++ spec/features/search_parking_spec.rb
@@ -8,7 +8,6 @@ RSpec.feature "Search Parking by Address", type: :feature do
     fill_in 'violation_address', :with => '11 broadway, m'
     keypress = "var e = $.Event('keydown', { keyCode: 13 }); $('body').trigger(e);"
     page.driver.execute_script(keypress) 
-    
     expect(find(:css, '.ui-menu-item').text).to eq("11 BROADWAY, MANHATTAN")
   end
 
@@ -17,7 +16,6 @@ RSpec.feature "Search Parking by Address", type: :feature do
     keypress = "var e = $.Event('keydown', { keyCode: 13 }); $('body').trigger(e);"
     page.driver.execute_script(keypress) 
     click_button "SEARCH"
-
     expect(page).to have_content("Total Violations Last Year: 30")
   end
 
---
+++ spec/features/send_sms_spec.rb
@@ -0,0 +1,8 @@
+require 'feature_helper'
+
+RSpec.feature "Search Parking by Address", type: :feature do 
+
+  scenario '', :js => true do
+  end
+  
+end
