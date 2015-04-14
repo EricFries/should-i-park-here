06c418bd97c09fcd1abefcebc1fd70a4de478255
ktravers <travers.kt@gmail.com>
2015-04-13 22:47:58 -0400

    fix button styling, refine sms action in text controller

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -4,29 +4,26 @@ class SendTextController < ApplicationController
   def sms
     number_to_send_to = params[:number]
     address = params[:address]
-    time_to_move = params[:time]
+    day_to_move = params[:day]
+    minute_to_move = params[:full_date_time]["hour(5i)"]
+    hour_to_move = params[:full_date_time]["hour(4i)"]
     am_pm = "am"
-    specific_hour_to_move = params[:specific_time]["hour(4i)"].to_i
-    
-    if specific_hour_to_move > 12
-      specific_hour_to_move -= 12 
+
+    if params[:full_date_time]["hour(4i)"].to_i > 12
+      hour_to_move = params[:full_date_time]["hour(4i)"].to_i - 12 
       am_pm = "pm"
     end
-    
-    specific_minute_to_move = params[:specific_time]["hour(5i)"]
   
     twilio_sid = Rails.application.secrets.twilio_sid
     twilio_token = Rails.application.secrets.twilio_token
-    twilio_phone_number = "9175123465"
- 
+    twilio_phone_number = Rails.application.secrets.twilio_number
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
       :to => number_to_send_to,
-      :body => "Don't forget to move your car!  It's parked at #{address}. Move it by #{specific_hour_to_move}:#{specific_minute_to_move} #{am_pm} on #{time_to_move}."
+      :body => "Don't forget to move your car!  It's parked at #{address}. Move it by #{hour_to_move}:#{minute_to_move} #{am_pm} on #{day_to_move}."
     )
-
     respond_to do |f|
       f.html
       f.js
