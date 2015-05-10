8a02daedf0681a13d92ded5143b997d410f3fc99
ktravers <travers.kt@gmail.com>
2015-05-10 17:42:07 -0400

    update sms controller; remove unused css

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -3,13 +3,9 @@ class SendTextController < ApplicationController
 
   def sms
     minute_to_move = params[:full_date_time]['hour(5i)']
-    hour_to_move = params[:full_date_time]['hour(4i)']
+    hour_to_move = params[:full_date_time]['hour(4i)'].to_i
     am_pm = 'am'
-
-    if params[:full_date_time]['hour(4i)'].to_i > 12
-      hour_to_move = params[:full_date_time]['hour(4i)'].to_i - 12 
-      am_pm = 'pm'
-    end
+    (hour_to_move -= 12; am_pm = 'pm') if hour_to_move > 12
 
     twilio_sid = Rails.application.secrets.twilio_sid
     twilio_token = Rails.application.secrets.twilio_token
@@ -18,8 +14,7 @@ class SendTextController < ApplicationController
     @twilio_client.account.sms.messages.create(
       from: "+1#{Rails.application.secrets.twilio_number}",
       to: params[:number],
-      body: "Don't forget to move your car! It's parked at #{params[:address]}. Move it by #{hour_to_move}:#{minute_to_move} #{am_pm} on #{params[:day]}."
-    )
+      body: "Don't forget to move your car! It's parked at #{params[:address]}. Move it by #{hour_to_move}:#{minute_to_move} #{am_pm} on #{params[:day]}.")
     respond_to do |f|
       f.html
       f.js
